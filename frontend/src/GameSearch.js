58c5ae7409f7bf771752f7cdf57216d36959bf56
Seongjae <robbyl95@hanmail.net>
2022-06-03 20:55:33 +0900

    잘되길ㅠ

--- frontend/src/GameSearch.js
+++ frontend/src/GameSearch.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Input } from 'antd';
 
 export default function GameSearch() {
 
@@ -25,7 +24,7 @@ export default function GameSearch() {
 
   return (
     <div>
-      <Input
+      <input
         placeholder='검색ㄱㄱ'
         onChange={(e) => searchItems(e.target.value)}
         style={{ width: 200 }}

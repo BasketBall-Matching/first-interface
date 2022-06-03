7bf04b33d8e6765ccbed678e6cee15191e14a5fa
devkya <kihaok@gmail.com>
2022-06-03 21:16:56 +0900

    asd

--- backend/game/models.py
+++ backend/game/models.py
@@ -3,7 +3,11 @@ from django.forms import CharField
 
 # Create your models here.
 class GuestGame(models.Model):
+    host = models.CharField(max_length=20)
     game_date = models.DateField()
     game_time = models.TimeField()
     location = models.CharField(max_length=100, blank=False, null=False)
     member = models.IntegerField()
+    
+    def __str__(self):
+        return '{} {}'.format(self.game_date, self.loacation)

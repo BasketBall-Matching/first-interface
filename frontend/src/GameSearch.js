9f9106152ee71812f6d4bc52052fddb7b1c735d4
Seongjae <robbyl95@hanmail.net>
2022-06-03 20:50:04 +0900

    get search frontend(미완)

---
+++ frontend/src/GameSearch.js
@@ -0,0 +1,64 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Input } from 'antd';
+
+export default function GameSearch() {
+
+  const [gameData, setGameData] = useState([]);
+  const [searchInput, setSearchInput] = useState('');
+  const [filteredResults, setFilteredResults] = useState([]);
+
+  useEffect(() => {
+    axios.get('/games')
+      .then((response) => {
+        setGameData(response.data);
+      });
+  }, []);
+
+  const searchItems = (searchValue) => {
+    setFilteredResults(filteredResults);
+    gameData.filter((game) => {
+      return Object.values(game).join('').includes(searchInput);
+    });
+    setSearchInput(searchValue);
+  };
+
+  return (
+    <div>
+      <Input
+        placeholder='검색ㄱㄱ'
+        onChange={(e) => searchItems(e.target.value)}
+        style={{ width: 200 }}
+      />
+      <div>
+        {searchInput.length > 1 ? (
+          filteredResults.map((game) => {
+            return (
+              <div>
+                <div style={{ color: 'green' }}>
+                  <div>{game.date}</div>
+                  <div>{game.time}</div>
+                  <div>{game.location}</div>
+                  <div>{game.member}</div>
+                </div>
+              </div>
+            );
+          })
+        ) : (
+          gameData.map((game) => {
+            return (
+              <div>
+                <div style={{ color: 'indigo' }}>
+                  <div>{game.date}</div>
+                  <div>{game.time}</div>
+                  <div>{game.location}</div>
+                  <div>{game.member}</div>
+                </div>
+              </div>
+            );
+          })
+        )}
+      </div>
+    </div>
+  );
+}
